109c5faaf1409087ab81f64c84b0fe0db38ba380
Mukul <mukulkumar627@gmail.com>
2022-04-14 15:08:08 +0530

    Search component framework

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -2,9 +2,21 @@ import React, { useState } from "react";
 import './Search.css';
 
 export default function Search() {
-    const [search, changeSearch] = useState();
+    const [search, setSearch] = useState("");
+    const [booksList, setList] = useState([]);
 
     return <div className="main-wrapper">
-        
+        <h3> Enter book to search for</h3>
+        <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+        /> 
+        <br/>
+        <br/>
+        <button onClick={(e) => {}}> Submit </button>
+
+        <div className="list">
+            {booksList.map((item,ind) => <div key={ind} className="item"> list item </div>)}
+        </div>
     </div>
 }
